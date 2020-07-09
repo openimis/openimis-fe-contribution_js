fff3af9b2cb4a7171700aa6161e4b37dcb377481
Xavier Gillmann <xgillmann@bluesquarehub.com>
2020-07-09 18:41:49 +0200

    init components for insurees & families

---
+++ src/components/FamilyPaymentsOverview.js
@@ -0,0 +1,41 @@
+import React, { Component, Fragment } from "react";
+import { withTheme, withStyles } from "@material-ui/core/styles";
+import { injectIntl } from 'react-intl';
+import _ from "lodash";
+import { Paper } from "@material-ui/core";
+import {
+    formatMessage, Table
+} from "@openimis/fe-core";
+
+
+const styles = theme => ({
+    paper: theme.paper.paper,
+    paperHeader: theme.paper.header,
+    paperHeaderAction: theme.paper.action, 
+    fab: theme.fab,   
+});
+
+class FamilyPaymentsOverview extends Component {
+
+    headers = [];
+
+    formatters = [];
+
+    render() {
+        const { intl, classes, edited, readOnly = false } = this.props;
+        return (
+            <Paper className={classes.paper}>
+
+                <Table
+                    module="contribution"
+                    header={formatMessage(intl, "contribution", "FamilyPayments")}
+                    headers={this.headers}
+                    itemFormatters={this.formatters}
+                    items={[]}
+                />
+            </Paper>
+        )
+    }
+}
+
+export default injectIntl(withTheme(withStyles(styles)(FamilyPaymentsOverview)));
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import messages_en from "./translations/en.json";
 
+import FamilyContributionsOverview from "./components/FamilyContributionsOverview";
+import FamilyPaymentsOverview from "./components/FamilyPaymentsOverview";
+
 const DEFAULT_CONFIG = {
   "translations": [{ key: "en", messages: messages_en }],
+  "insuree.FamilyOverview.panels": [FamilyContributionsOverview, FamilyPaymentsOverview],
 }
 
 export const ContributionModule = (cfg) => {

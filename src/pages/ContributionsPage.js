26a3dc23a779e49c86843e92e040d8ecb8cdd1de
olewandowski <olewandowski@soldevelo.com>
2023-03-21 15:08:58 +0100

    OTC-757: Clearing pagination state if entered the ContributionsPage

--- src/pages/ContributionsPage.js
+++ src/pages/ContributionsPage.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
+import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import { injectIntl } from 'react-intl';
 import { withTheme, withStyles } from "@material-ui/core/styles";
-import { historyPush, withModulesManager, withHistory } from "@openimis/fe-core"
+import { historyPush, withModulesManager, withHistory, clearCurrentPaginationPage } from "@openimis/fe-core"
 import ContributionSearcher from "../components/ContributionSearcher";
 
 
@@ -17,6 +18,12 @@ class ContributionsPage extends Component {
     onDoubleClick = (c, newTab = false) => {
         historyPush(this.props.modulesManager, this.props.history, "contribution.contributionOverview", [c.uuid], newTab)
     }
+
+    componentDidMount = () => {
+        const moduleName = "contribution";
+        const { module } = this.props;
+        if (module !== moduleName) this.props.clearCurrentPaginationPage();
+      };
 
     render() {
         const { classes } = this.props;
@@ -33,8 +40,11 @@ class ContributionsPage extends Component {
 
 const mapStateToProps = state => ({
     rights: !!state.core && !!state.core.user && !!state.core.user.i_user ? state.core.user.i_user.rights : [],
+    module: state.core?.savedPagination?.module,
 })
 
+const mapDispatchToProps = (dispatch) => bindActionCreators({ clearCurrentPaginationPage }, dispatch);
+
 export default injectIntl(withModulesManager(
-    withHistory(connect(mapStateToProps)(withTheme(withStyles(styles)(ContributionsPage))))
+    withHistory(connect(mapStateToProps, mapDispatchToProps)(withTheme(withStyles(styles)(ContributionsPage))))
 ));

b78293c3ea03faafcd90626673265d56389cd7ce
olewandowski <olewandowski@soldevelo.com>
2023-03-21 15:11:37 +0100

    OTC-757: Code refactor in ContributionsPage

--- src/pages/ContributionsPage.js
+++ src/pages/ContributionsPage.js
@@ -1,50 +1,71 @@
 import React, { Component } from "react";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
-import { injectIntl } from 'react-intl';
+import { injectIntl } from "react-intl";
+
 import { withTheme, withStyles } from "@material-ui/core/styles";
-import { historyPush, withModulesManager, withHistory, clearCurrentPaginationPage } from "@openimis/fe-core"
+
+import {
+  historyPush,
+  withModulesManager,
+  withHistory,
+  clearCurrentPaginationPage,
+} from "@openimis/fe-core";
 import ContributionSearcher from "../components/ContributionSearcher";
 
-
-const styles = theme => ({
-    page: theme.page,
-    fab: theme.fab
+const styles = (theme) => ({
+  page: theme.page,
+  fab: theme.fab,
 });
 
-
 class ContributionsPage extends Component {
+  onDoubleClick = (c, newTab = false) => {
+    historyPush(
+      this.props.modulesManager,
+      this.props.history,
+      "contribution.contributionOverview",
+      [c.uuid],
+      newTab
+    );
+  };
 
-    onDoubleClick = (c, newTab = false) => {
-        historyPush(this.props.modulesManager, this.props.history, "contribution.contributionOverview", [c.uuid], newTab)
-    }
+  componentDidMount = () => {
+    const moduleName = "contribution";
+    const { module } = this.props;
+    if (module !== moduleName) this.props.clearCurrentPaginationPage();
+  };
 
-    componentDidMount = () => {
-        const moduleName = "contribution";
-        const { module } = this.props;
-        if (module !== moduleName) this.props.clearCurrentPaginationPage();
-      };
-
-    render() {
-        const { classes } = this.props;
-        return (
-            <div className={classes.page}>
-                <ContributionSearcher
-                    cacheFiltersKey="contributionsPageFiltersCache"
-                    onDoubleClick={this.onDoubleClick}
-                />
-            </div>
-        )
-    }
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.page}>
+        <ContributionSearcher
+          cacheFiltersKey="contributionsPageFiltersCache"
+          onDoubleClick={this.onDoubleClick}
+        />
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => ({
-    rights: !!state.core && !!state.core.user && !!state.core.user.i_user ? state.core.user.i_user.rights : [],
-    module: state.core?.savedPagination?.module,
-})
+const mapStateToProps = (state) => ({
+  rights:
+    !!state.core && !!state.core.user && !!state.core.user.i_user
+      ? state.core.user.i_user.rights
+      : [],
+  module: state.core?.savedPagination?.module,
+});
 
-const mapDispatchToProps = (dispatch) => bindActionCreators({ clearCurrentPaginationPage }, dispatch);
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators({ clearCurrentPaginationPage }, dispatch);
 
-export default injectIntl(withModulesManager(
-    withHistory(connect(mapStateToProps, mapDispatchToProps)(withTheme(withStyles(styles)(ContributionsPage))))
-));
+export default injectIntl(
+  withModulesManager(
+    withHistory(
+      connect(
+        mapStateToProps,
+        mapDispatchToProps
+      )(withTheme(withStyles(styles)(ContributionsPage)))
+    )
+  )
+);

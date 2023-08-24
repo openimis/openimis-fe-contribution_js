490723d0fa2479e1dc5c98a834e3bfe2ad31b8f2
Thibault Dethier <tdethier@bluesquarehub.com>
2023-08-24 17:15:17 +0200

    OMT-328 - Contributions Distribution report

---
+++ src/reports/ContributionsDistribution.js
@@ -0,0 +1,81 @@
+import { Grid } from "@material-ui/core";
+import { PublishedComponent, useModulesManager, useTranslations } from "@openimis/fe-core";
+import React from "react";
+
+const ContributionsDistributionReport = (props) => {
+  const { values, setValues } = props;
+  const modulesManager = useModulesManager();
+  const { formatMessage } = useTranslations("contribution", modulesManager);
+
+  return (
+    <Grid container direction="column" spacing={1}>
+      <Grid item>
+        <PublishedComponent
+          pubRef="core.YearPicker"
+          onChange={(year) =>
+            setValues({
+                ...values,
+                year,
+          })}
+          min={2010}
+          max={2040}
+          required
+          withNull={false}
+          value={values.year}
+        />
+      </Grid>
+      <Grid item>
+        <PublishedComponent
+          pubRef="product.ProductPicker"
+          onChange={(product) => setValues({ ...values, product })}
+          module="contribution"
+          required
+          value={values.product}
+          label={formatMessage("ContributionsDistributionReport.product")}
+        />
+      </Grid>
+      <Grid item>
+        <PublishedComponent
+          pubRef="location.LocationPicker"
+          onChange={(region) =>
+            setValues({
+                ...values,
+                region,
+                district:null,
+          })}
+          value={values.region}
+          locationLevel={0}
+          label={formatMessage("ContributionsDistributionReport.region")}
+        />
+      </Grid>
+      <Grid item>
+        <PublishedComponent
+          pubRef="location.LocationPicker"
+          onChange={(district) =>
+            setValues({
+                ...values,
+                district,
+          })}
+          value={values.district}
+          parentLocation={values.region}
+          locationLevel={1}
+          label={formatMessage("ContributionsDistributionReport.district")}
+        />
+      </Grid>
+      <Grid item>
+        <PublishedComponent
+          pubRef="core.MonthPicker"
+          onChange={(month) =>
+            setValues({
+                ...values,
+                month,
+          })}
+          withNull
+          value={values.month}
+        />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default ContributionsDistributionReport;
